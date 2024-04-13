aec6417593f23354ca62c54fc1f8d47c0c2096d4
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2024-04-13 14:35:07 +0300

    fix unit and ui tests

--- tests/ui/footer.test.js
+++ tests/ui/footer.test.js
@@ -1,9 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
-test('Check footer', async ({ page }) => {
-    await page.goto('http://localhost:8080');  
+test('Check footer', async({ page }) => {
+    await page.goto('http://localhost:8080');
     const footer = await page.$('footer');
     const text = await footer.textContent();
-    expect(text).toContain('© 2023 - Software Engineering and DevOps exam preparation.');
-  });
-  
+    expect(text).toContain('© 2024 - Software Engineering and DevOps regular exam');
+});
--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,8 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
-test('Check cookbook page', async ({ page }) => {
+test('Check cookbook page', async({ page }) => {
     await page.goto('http://localhost:8080/cookbook');
-    const list = await page.$('dl');
+    const list = await page.$('ul');
     expect(list).toBeTruthy();
-  });
-  
+});
